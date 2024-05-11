d213ef6a16f1921bed208b154fb351a9cb2f09f1
fantiyun <mrzhanggy@aliyun.com>
2024-05-11 11:50:51 +0800

    feat: feature user data management

---
+++ vibes-express/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs')
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
+
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+    try {
+        let dataJSON = await readFile('./fixtures/db1.json', 'utf8')
+        res.send(JSON.parse(dataJSON).users)
+    } catch (error) {
+        res.status(500).json({error})
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

0da3c2f2354775d26676cf300646fd137a6f7744
fantiyun <mrzhanggy@aliyun.com>
2024-05-15 02:21:53 +0800

    feat: mongodb 增删改查

--- vibes-express/index.js
+++ vibes-express/index.js
@@ -21,10 +21,38 @@ const main = async () => {
   //   console.log('insertData', insertData)
 
   //   添加多条数据
-  await readFile('./fixtures/db.json', 'utf8', async (err, data) => {
-    const insertedData = await users.insertMany(JSON.parse(data))
-    console.log(insertedData)
-  })
+  //   await readFile('./fixtures/db.json', 'utf8', async (err, data) => {
+  //     const insertedData = await users.insertMany(JSON.parse(data))
+  //     console.log(insertedData)
+  //   })
+
+  //   查询一条
+  //   const userinfo = await users.findOne({ age: { $gt: 18 } })
+  //   console.log('userinfo', userinfo)
+
+  //   查询多条
+  //   const userinfo = await users.find({ age: { $gt: 18 } })
+  //   console.log('userinfo', await userinfo.toArray())
+
+  //   更新一条
+  //   const userinfo = await users.updateOne(
+  //     { age: 18 },
+  //     { $set: { username: 'fantiyun' } }
+  //   )
+  //   console.log('userinfo', userinfo)
+
+  //   更新多条
+  //   const userinfo = await users.updateMany(
+  //     { age: 20 },
+  //     { $set: { username: 'update-name' } }
+  //   )
+  //   console.log('userinfo', userinfo)
+
+  //   删除一条
+  //   const deleted = await users.deleteOne({ username: 'fantiyun' })
+  //   删除多条
+  const deletedAll = await users.deleteMany({ age: { $lt: 100 } })
+  console.log(deletedAll)
 }
 
 main().finally(() => client.close())
